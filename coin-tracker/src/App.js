557ce7bc72c3c65589b7f36ac930d9ead1145c99
johyewon <ixxwxfjlwf@gmail.com>
2021-11-29 13:38:19 +0900

    view more better

--- coin-tracker/src/App.js
+++ coin-tracker/src/App.js
@@ -3,13 +3,35 @@ import {useEffect, useState} from "react";
 function App() {
     const [loading, setLoading] = useState(true);
     const [coins, setCoins] = useState([]);
-    const [money, setMoney] = useState();
 
-    const onChange = (event) => {
-        if(event.target.value !== undefined && event.target.value >= 0)
-            setMoney(event.target.value);
+
+    function CoinData() {
+        const [money, setMoney] = useState();
+
+        const onChange = (event) => {
+            if (event.target.value !== undefined && event.target.value >= 0)
+                setMoney(event.target.value);
+        }
+
+        return (
+            <div>
+                <input type="number"
+                        value={money}
+                        onChange={onChange}
+                        placeholder="Please input your USD"/>
+                <ul>
+                    {coins.map((coin) =>
+                        <li key={coin.id}>{coin.name} ({coin.symbol}) :
+                            {money ? Math.round(money / coin.quotes.USD.price) : 0} USD</li>
+                    )}
+                </ul>
+            </div>
+        );
     }
 
+    function Loading() {
+        return (<strong>Loading...</strong>);
+    }
 
     useEffect(() => {
         fetch("https://api.coinpaprika.com/v1/tickers")
@@ -23,17 +45,7 @@ function App() {
     return (
         <div>
             <h1>The Coins! ({coins.length})</h1>
-            {loading ? <strong>Loading...</strong> : null}
-            <input type="number"
-                   value={money}
-                   onChange={onChange}
-                   placeholder="Please input your USD"/>
-            <ul>
-                {coins.map((coin) =>
-                    <li key={coin.id}>{coin.name} ({coin.symbol}) :
-                        {money ? Math.round(money / coin.quotes.USD.price) : 0} USD</li>
-                )}
-            </ul>
+            {loading ? <Loading /> : <CoinData />}
         </div>
     );
 }
