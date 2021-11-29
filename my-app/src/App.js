e0c4205c3ae0bfe1a9b22b87eb4d895b5e4054fd
johyewon <ixxwxfjlwf@gmail.com>
2021-11-29 10:17:40 +0900

    Effect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,40 @@
-import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev +1); 
-  console.log("call an api");
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log("i run all the time");
+
+  useEffect(() => {
+    // 코드를 한 번만 렌더링할 수 있게 도와줌
+    console.log("CALL THE API...");
+  }, []);
+
+  useEffect(() => {
+    // keyword 가 변경될 때에만 렌더링할 수  있게 도와줌
+    if (keyword !== "" && keyword.length > 5)
+      console.log("SEARCH FOR", keyword);
+  }, [keyword]);
+
+  useEffect(() => {
+    // counter 가 변경될 때에만 렌더링할 수  있게 도와줌
+      console.log("counter ", counter);
+  }, [counter]);
 
   return (
     <div>
-      <h1 className=
-      {styles.title}>{counter}</h1>
-      <Button text="Click Me" onClick={onClick}/>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 }
