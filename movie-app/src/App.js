603fe36ee6881b018deda69c4cf66b2de37aee7f
johyewon <ixxwxfjlwf@gmail.com>
2021-11-29 17:48:58 +0900

    folder + Detail Paging

---
+++ movie-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
+function App() {
+    return <BrowserRouter>
+        <Switch>
+            <Route path="/movie/:id">
+                <Detail />
+            </Route>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Switch>
+    </BrowserRouter>;
+}
+
+export default App;
---
+++ movie-app/src/routes/Detail.js
@@ -0,0 +1,28 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+
+function Detail() {
+    const [movieData, setMovieData] = useState([]);
+
+    const {id} = useParams();
+
+    const getMovieDetail = async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
+
+        setMovieData(json.data.movie);
+    }
+
+    useEffect(() => {
+        getMovieDetail();
+    }, []);
+
+    return (
+        <h1>Detail</h1>
+        <div>
+            {movie}
+        </div>
+    );
+}
+
+export default Detail;
