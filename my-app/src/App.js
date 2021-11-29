5bbab2474bf78b50e384d8dfee913602184eaa97
johyewon <ixxwxfjlwf@gmail.com>
2021-11-29 10:51:28 +0900

    cleanUp

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,29 @@
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+function Hello() {
+
+  // cleanUp Function
+  function byeFn() {
+    console.log("bye :( ");
+  }
+  function hiFn() {
+    console.log("create :)");
+    return byeFn;
+  }
+
+  useEffect(hiFn, []);
+
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log("i run all the time");
-
-  useEffect(() => {
-    // 코드를 한 번만 렌더링할 수 있게 도와줌
-    console.log("CALL THE API...");
-  }, []);
-
-  useEffect(() => {
-    // keyword 가 변경될 때에만 렌더링할 수  있게 도와줌
-    if (keyword !== "" && keyword.length > 5)
-      console.log("SEARCH FOR", keyword);
-  }, [keyword]);
-
-  useEffect(() => {
-    // counter 가 변경될 때에만 렌더링할 수  있게 도와줌
-      console.log("counter ", counter);
-  }, [counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>Click Me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
