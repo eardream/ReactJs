5a712f98d00421c542816d4576251ac1d0bc0880
johyewon <ixxwxfjlwf@gmail.com>
2021-12-07 13:23:20 +0900

    iOS version crash

--- ReactNative/movvies/App.js
+++ ReactNative/movvies/App.js
@@ -4,7 +4,7 @@ import AppLoading from "expo-app-loading";
 import {Ionicons} from "@expo/vector-icons";
 import {useAssets} from "expo-asset";
 import {NavigationContainer} from "@react-navigation/native";
-import {useColorScheme, StyleSheet} from "react-native";
+import {useColorScheme} from "react-native";
 import Root from "./navigation/Root";
 import {darkTheme, lightTheme} from "./styled";
 import {ThemeProvider} from "styled-components";
@@ -20,6 +20,7 @@ export default function App() {
             <AppLoading/>
         );
     }
+
     return (
         <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
             <NavigationContainer>
--- ReactNative/movvies/screens/Movies.js
+++ ReactNative/movvies/screens/Movies.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import styled from "styled-components/native";
+import Swiper from "react-native-web-swiper";
+import {Dimensions} from "react-native";
 
-const Btn = styled.TouchableOpacity`
+const ScrollView = styled.ScrollView`
+  background-color: ${props => props.theme.mainBgColor};
+`;
+
+const View = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.mainBgColor};
+
 `;
 
-const Title = styled.Text`
-  color: ${props => props.theme.textColor};
-`;
+const {height: SCREEN_HEIGHT} = Dimensions.get("window");
 
 const Movies = ({navigation: {navigate}}) => (
-    <Btn onPress={() => navigate("Stack", {screen: "Three"})}>
-        <Title>Movie</Title>
-    </Btn>
+    <ScrollView>
+        <Swiper
+            loop={true}
+            timeout={3.5}
+            controlsEnabled={false}
+            containerStyle={{width: "100%", height: SCREEN_HEIGHT / 4}}>
+        </Swiper>
+    </ScrollView>
 );
 export default Movies;
