bae90800d09e13ff9a6255755d5b85d46840cad7
johyewon <ixxwxfjlwf@gmail.com>
2021-12-13 17:51:08 +0900

    auto animation + interpola animation

--- ReactNative/langApp/App.js
+++ ReactNative/langApp/App.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef } from "react";
+import { Animated, Dimensions } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -7,34 +8,89 @@ const Container = styled.View`
   align-items: center;
 `;
 
-const Box = styled.TouchableOpacity`
-  background-color: lightblue;
+const Touchable = styled.Pressable`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled.View`
+  background-color: "rgb(125, 154, 255)";
   width: 200px;
   height: 200px;
 `;
+const AnimateBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  const [y, setY] = useState(0);
-  const [intervalId, setIntervalId] = useState(null);
+  const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } =
+    Dimensions.get("window");
+
+  const position = useRef(
+    new Animated.ValueXY({
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    })
+  ).current;
 
   const moveUp = () => {
-    const id = setInterval(() => setY((prev) => prev + 1), 10);
-    setIntervalId(id);
+    Animated.loop(
+      Animated.sequence([bottomLeft, bottomRight, topRight, topLeft])
+    ).start();
   };
 
-  useEffect(() => {
-    if (y === 200) {
-      clearInterval(intervalId);
-    }
-  }, [y, intervalId]);
+  const topLeft = Animated.timing(position, {
+    toValue: {
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    },
+    useNativeDriver: false,
+  });
+
+  const topRight = Animated.timing(position, {
+    toValue: {
+      x: SCREEN_WIDTH / 2 - 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    },
+    useNativeDriver: false,
+  });
+
+  const bottomLeft = Animated.timing(position, {
+    toValue: {
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: SCREEN_HEIGHT / 2 - 100,
+    },
+    useNativeDriver: false,
+  });
+
+  const bottomRight = Animated.timing(position, {
+    toValue: {
+      x: SCREEN_WIDTH / 2 - 100,
+      y: SCREEN_HEIGHT / 2 - 100,
+    },
+    useNativeDriver: false,
+  });
+
+  // y 값의 변경에 따라 box 의 style value를 interpolate 를 사용해 조절
+  const borderRadius = position.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: [100, 10],
+  });
+
+  const rotation = position.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ["-360deg", "360deg"],
+  });
+
   return (
     <Container>
-      <Box
-        onPress={moveUp}
-        style={{
-          transform: [{ translateY: y }],
-        }}
-      />
+      <Touchable onPress={moveUp}>
+        <AnimateBox
+          style={{
+            borderRadius: borderRadius,
+            transform: [...position.getTranslateTransform()],
+          }}
+        />
+      </Touchable>
     </Container>
   );
 }
